693dd149018e22119ae94119f603642d73ae6a46
danglduy <38782010+danglduy@users.noreply.github.com>
2020-06-08 10:42:42 +0700

    update gems & rubocop

--- template.rb
+++ template.rb
@@ -9,9 +9,9 @@ g.options = options.merge(app_name: app_name, rails_generator: self)
 after_bundle do
   g.create_initial_commit
   g.exclude_database_yml
-  g.install_rspec 3.9
-  g.install_capybara 3.31
-  g.install_factory_bot 5.1
-  g.install_shoulda_matchers 4.2
+  g.install_rspec 4.0
+  g.install_capybara 3.32
+  g.install_factory_bot 5.2
+  g.install_shoulda_matchers 4.3
   g.install_rubocop
 end
